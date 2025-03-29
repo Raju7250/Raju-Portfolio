b90a092ce69c356328991e85ff6a58efd50ef345
Raju Kumar Gupta <raju.kumar@sifars.com>
2025-03-29 18:00:12 +0530

    Update Home component description and modify experience entries for accuracy

--- src/constants.tsx
+++ src/constants.tsx
@@ -18,28 +18,31 @@ export const name = 'Raju Gupta';
 
 export const experiences = [
     {
-        'company': 'Inncircles Technologies',
-        'role': 'Product Developer',
-        'duration': 'April 2023 - Present',
-        'logo': 'https://images.crunchbase.com/image/upload/c_pad,f_auto,q_auto:eco,dpr_1/e5gz1yggdy8sc1umna3e',
+        'company': 'MAQ Software',
+        'role': 'Software Developer',
+        'duration': 'Jan 2024 - Present',
+        'logo': 'https://www.maqsoftware.com/img/MAQSoftware.png',
         'points': [
-            'Involved in dynamic contributions to web and mobile📱 development projects, actively influencing and improving project outcomes.',
-            'Developed multiple shared components to simplify tasks for fellow developers, boosting efficiency and teamwork.',
-            'Enhanced product performance through significant contributions to backend development, optimizing essential components for improved efficiency 📈.',
-            'Implemented data security for the company’s product to provide role-based access control ⚙️.'
+            'Integrated and customized Power BI reports into web applications using React and TypeScript, adding interactive features and controls beyond the default options. Leveraged advanced React techniques like hooks and context to create a seamless and engaging user interface, enhancing the overall user experience.',
+            'Developed a user-friendly training section component for internal projects, ensuring cross-device compatibility, which resulted in a 40% decrease in user support inquiries and significantly improved the educational experience.',
+            'Implemented a custom Power BI report viewer using React and TypeScript, allowing users to view reports without needing a Power BI account. This solution improved accessibility and user engagement with the reports.',
+            'Implemented a new UI version for a major enterprise platform as part of a large-scale modernization project, enhancing usability and user experience. Collaborated with cross-functional teams to gather requirements, design solutions, and integrate new functionalities, ensuring the timely delivery of high-quality software.'
         ],
-        'url': 'https://inncircles.com/#home',
+        'url': 'https://maqsoftware.com/',
     },
     {
-        'company': 'AT & T',
-        'role': 'Technical Intern',
-        'duration': 'Jan 2023 - March 2023',
-        'logo': 'https://www.att.com/ecms/dam/att/consumer/global/logos/att_globe_500x500.jpg',
+        'company': 'Sifars',
+        'role': 'Associate Software Developer',
+        'duration': 'Jun 2023 - Dec 2023',
+        'logo': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1AEW7A_uqJpmjnEAs8OsaMNG1YuoxqLpEWA&s',
         'points': [
             'Gained experience with Git version control contributing to the development and maintenance of code repositories.',
-            'Worked on the application dashboard, making it more user-friendly and improving its overall functionality📱.'
+            'Worked with Agile methodologies, participating in daily stand-ups and sprint planning sessions to ensure timely delivery of features and improvements.',
+            'Implemented user and role management features, including secure authentication, profile updates, and role-based access control with permissions.',
+            'Developed and maintained RESTful APIs for seamless communication between frontend and backend systems, ensuring efficient data retrieval and manipulation.',
+            'Designed a call log and attachment tracking system, improving reporting accuracy by 50% and enabling better data-driven decision-making within the customer service application.'
         ],
-        'url': 'https://www.att.com/',
+        'url': 'https://www.sifars.com/en/',
     },
 ]
 
